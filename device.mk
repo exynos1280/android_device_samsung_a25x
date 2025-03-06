6652ce0944a66c39cd51d130872cd86a6422082e
Le Hong Duc <hongducthbk123@gmail.com>
2025-03-06 19:15:23 +0100

    a25x: init removepackages
    Signed-off-by: Onelots <onelots@onelots.fr>
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 DEVICE_PATH := device/samsung/a25x
 
+PRODUCT_PACKAGES += \
+    RemovePackagesa25x
+
 # Audio - Configuration
 PRODUCT_PACKAGES += \
     audio_board_info.xml \
