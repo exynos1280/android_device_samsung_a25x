7b64d6238175714e0e56c8c6d59e5c739bc9aec5
Koushik Dey <kousickdey3@gmail.com>
2025-07-08 23:13:51 +0530

    a25x: Disable Matlog
    * Disable Matlog if the build variant is 'user' to prevent log access in production builds
    
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Change-Id: I1d4a0ecdba1f029340e3ca2486e7a663a58c753b

--- device.mk
+++ device.mk
@@ -48,6 +48,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/init/init.a25x.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.a25x.rc \
     $(DEVICE_PATH)/configs/init/init.a25x.unify.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.a25x.unify.rc
 
+# Matlog
+ifeq ($(TARGET_BUILD_VARIANT),user)
+TARGET_DISABLE_MATLOG := true
+endif
+
 # Nfc
 PRODUCT_PACKAGES += android.hardware.nfc@1.2-service.st
 
