804be5b78ce2a4a92735fc4645804f63c7c82e29
Flopster101 <nahuelgomez329@gmail.com>
2026-08-05 21:05:47 -0300

    a25x: Set SoundBooster DSP library name
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,8 @@ endif
 # Audio - Configuration
 PRODUCT_PACKAGES += mixer_paths.xml
 
+$(call soong_config_set,s5e8825,soundbooster_dsp_library,//vendor/samsung/a25x:lib_SoundBooster_ver1130)
+
 # FM Radio
 PRODUCT_PACKAGES += \
     RevampedFMRadio \
